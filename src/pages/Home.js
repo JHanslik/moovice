0d02923315d4f0c740877164e4663cc00138d237
JHanslik <julien.hanslik@gmail.com>
2022-09-26 15:38:54 +0200

    latest deleted, no real use

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,25 +3,25 @@ import PreviewCards from "../components/PreviewCards";
 import { Link } from "react-router-dom";
 
 function Home(props) {
-    const [latestMovie, setLatestMovies] = useState([]);
+    // const [latestMovie, setLatestMovies] = useState([]);
     const [topMovies, setTopMovies] = useState([]);
     const [nowPlayingMovies, setNowPlayingMovies] = useState([]);
     const [upcomingMovies, setUpcomingMovies] = useState([]);
 
     useEffect(() => {
-        fetchLatest();
+        // fetchLatest();
         fetchTop();
         fetchNowPlaying();
         fetchUpcoming();
     }, []);
 
-    const fetchLatest = async () => {
-        const request = await fetch(
-            `https://api.themoviedb.org/3/movie/latest?api_key=c553055e26e069d72e96bea7b56dc984`
-        );
-        const response = await request.json();
-        setLatestMovies(response);
-    };
+    // const fetchLatest = async () => {
+    //     const request = await fetch(
+    //         `https://api.themoviedb.org/3/movie/latest?api_key=c553055e26e069d72e96bea7b56dc984`
+    //     );
+    //     const response = await request.json();
+    //     setLatestMovies(response);
+    // };
 
     const fetchTop = async () => {
         const request = await fetch(
